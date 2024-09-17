a17b199b4f4b251c2e9ce5d2dfd99a71f8c6d8e0
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-09-17 12:46:13 +0530

    Create 17-09-2024.py

---
+++ Streak/2024/September/17-09-2024.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        return [key for key,value in Counter(s1.split() + s2.split()).items() if value == 1]
+
+        
