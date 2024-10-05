10513ca861b9d8f3ab7f06e6296d79429b37e370
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2024-10-05 15:23:58 +0530

    Create 05-10-2024.py

---
+++ Streak/2024/October/05-10-2024.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkInclusion(self, p: str, s: str) -> bool:
+        if len(p) > len(s): return False
+        f1, f2 = [0] * 26, [0] * 26
+        for i in range(len(p)):
+            f1[ord(p[i]) - ord('a')] += 1
+            f2[ord(s[i]) - ord('a')] += 1
+        for i in range(len(p), len(s)):
+            if f1 == f2: return True
+            f2[ord(s[i]) - ord('a')] += 1
+            f2[ord(s[i - len(p)]) - ord('a')] -= 1
+        return f1 == f2
