ae8f9d384cd6282c3c8b4400b8da94b272f24b8d
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2025-04-06 09:58:32 +0530

    Create 06-04-2025.py

---
+++ Streak/2025/April/06-04-2025.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+        n = len(nums)
+        subset = []
+        nums.sort()
+        dp = [1] * n
+        prev = [-1] * n
+        max_index = 0
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > dp[max_index]:
+                max_index = i
+        i = max_index
+        while i >= 0:
+            subset.append(nums[i])
+            i = prev[i]
+        
+        subset.reverse()
+        return subset        
